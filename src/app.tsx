c7d2086b1e949c6d85fb8f8a2490bdd79c5f6dee
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-22 21:36:25 -0300

    add miragejs

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,25 @@ import {Dashboard} from 'components/dashboard'
 import {Header} from 'components/header'
 import {GlobalStyle} from 'styles/global'
 
+import {createServer} from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'withdraw',
+          category: 'Food'
+        }
+      ]
+    })
+  }
+})
 
 export const App = () => (
   <>
--- src/components/transactions-table/index.tsx
+++ src/components/transactions-table/index.tsx
@@ -1,6 +1,14 @@
+import {useEffect} from 'react'
+
 import * as S from './styles'
 
 export const TransactionsTable = () => {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(r => r.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <S.Wrapper>
       <thead>
