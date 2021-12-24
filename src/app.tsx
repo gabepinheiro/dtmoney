6e67305d359c0ecc180b404cd04387660567e830
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-23 21:01:02 -0300

    add seeds on server api

--- src/app.tsx
+++ src/app.tsx
@@ -12,8 +12,30 @@ createServer({
     transaction: Model
   },
 
-  routes() {
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev.',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Despesas',
+          amount: 11000,
+          createdAt: new Date('2021-02-14 11:00:00')
+        }
+      ]
+    })
+  },
 
+  routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
